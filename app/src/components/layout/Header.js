6be1c16b1d1194fe6b263d09bbe273ef667d8774
leggio <harrison@legg.io>
2022-08-20 14:31:26 -0500

    change primary color

--- app/src/components/layout/Header.js
+++ app/src/components/layout/Header.js
@@ -1,24 +1,24 @@
-import React, { useState, useRef, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { Link } from 'react-router-dom';
-import Logo from './partials/Logo';
+import React, { useState, useRef, useEffect } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { Link } from "react-router-dom";
+import Logo from "./partials/Logo";
 
 const propTypes = {
   navPosition: PropTypes.string,
   hideNav: PropTypes.bool,
   hideSignin: PropTypes.bool,
   bottomOuterDivider: PropTypes.bool,
-  bottomDivider: PropTypes.bool
-}
+  bottomDivider: PropTypes.bool,
+};
 
 const defaultProps = {
-  navPosition: '',
+  navPosition: "",
   hideNav: false,
   hideSignin: false,
   bottomOuterDivider: false,
-  bottomDivider: false
-}
+  bottomDivider: false,
+};
 
 const Header = ({
   className,
@@ -29,7 +29,6 @@ const Header = ({
   bottomDivider,
   ...props
 }) => {
-
   const [isActive, setIsactive] = useState(false);
 
   const nav = useRef(null);
@@ -37,56 +36,59 @@ const Header = ({
 
   useEffect(() => {
     isActive && openMenu();
-    document.addEventListener('keydown', keyPress);
-    document.addEventListener('click', clickOutside);
+    document.addEventListener("keydown", keyPress);
+    document.addEventListener("click", clickOutside);
     return () => {
-      document.removeEventListener('keydown', keyPress);
-      document.removeEventListener('click', clickOutside);
+      document.removeEventListener("keydown", keyPress);
+      document.removeEventListener("click", clickOutside);
       closeMenu();
     };
-  });  
+  });
 
   const openMenu = () => {
-    document.body.classList.add('off-nav-is-active');
-    nav.current.style.maxHeight = nav.current.scrollHeight + 'px';
+    document.body.classList.add("off-nav-is-active");
+    nav.current.style.maxHeight = nav.current.scrollHeight + "px";
     setIsactive(true);
-  }
+  };
 
   const closeMenu = () => {
-    document.body.classList.remove('off-nav-is-active');
+    document.body.classList.remove("off-nav-is-active");
     nav.current && (nav.current.style.maxHeight = null);
     setIsactive(false);
-  }
+  };
 
   const keyPress = (e) => {
     isActive && e.keyCode === 27 && closeMenu();
-  }
+  };
 
   const clickOutside = (e) => {
-    if (!nav.current) return
-    if (!isActive || nav.current.contains(e.target) || e.target === hamburger.current) return;
+    if (!nav.current) return;
+    if (
+      !isActive ||
+      nav.current.contains(e.target) ||
+      e.target === hamburger.current
+    )
+      return;
     closeMenu();
-  }  
+  };
 
   const classes = classNames(
-    'site-header',
-    bottomOuterDivider && 'has-bottom-divider',
+    "site-header",
+    bottomOuterDivider && "has-bottom-divider",
     className
   );
 
   return (
-    <header
-      {...props}
-      className={classes}
-    >
+    <header {...props} className={classes}>
       <div className="container">
-        <div className={
-          classNames(
-            'site-header-inner',
-            bottomDivider && 'has-bottom-divider'
-          )}>
+        <div
+          className={classNames(
+            "site-header-inner",
+            bottomDivider && "has-bottom-divider"
+          )}
+        >
           <Logo />
-          {!hideNav &&
+          {!hideNav && (
             <>
               <button
                 ref={hamburger}
@@ -100,34 +102,44 @@ const Header = ({
               </button>
               <nav
                 ref={nav}
-                className={
-                  classNames(
-                    'header-nav',
-                    isActive && 'is-active'
-                  )}>
+                className={classNames("header-nav", isActive && "is-active")}
+              >
                 <div className="header-nav-inner">
-                  <ul className={
-                    classNames(
-                      'list-reset text-xs',
+                  <ul
+                    className={classNames(
+                      "list-reset text-xs",
                       navPosition && `header-nav-${navPosition}`
-                    )}>
-                  </ul>
-                  {!hideSignin &&
-                    <ul
-                      className="list-reset header-nav-right"
-                    >
+                    )}
+                  ></ul>
+                  {!hideSignin && (
+                    <ul className="list-reset header-nav-right">
                       <li>
-                        <a href="https://www.github.com/pop-punk/ethmexico" className="button button-primary button-wide-mobile button-sm" target="_blank">💻 View the code</a>
+                        <a
+                          href="https://www.github.com/pop-punk/ethmexico"
+                          className="button button-primary button-wide-mobile button-sm"
+                          target="_blank"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                          </svg>
+                        </a>
                       </li>
-                    </ul>}
+                    </ul>
+                  )}
                 </div>
               </nav>
-            </>}
+            </>
+          )}
         </div>
       </div>
     </header>
   );
-}
+};
 
 Header.propTypes = propTypes;
 Header.defaultProps = defaultProps;
